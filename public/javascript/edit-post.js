ff7d61d5115d34c356b27ae795fddb180280d935
micah_z <micah.a.zahn@gmail.com>
2022-06-04 18:53:07 -0500

    add delete post functionality

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -25,5 +25,27 @@ async function editFormHandler(event) {
       alert(response.statusText);
     }
   }
+
+  async function deleteBtnHandler(event) {
+    event.preventDefault();
+
+    // get post id
+    const urlArray = window.location.toString().split('/');
+    const id = urlArray[urlArray.length - 1];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
   
   document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  document.querySelector('#delete-btn').addEventListener('click',
+  deleteBtnHandler);
